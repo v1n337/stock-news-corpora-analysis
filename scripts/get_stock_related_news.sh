410d9558393efdd08366dfb06e19214f649c7f8d
Vineet John <vinjohn10@gmail.com>
2017-04-12 19:49:58 -0400

    Fixed parsing bug

--- scripts/get_stock_related_news.sh
+++ scripts/get_stock_related_news.sh
@@ -3,13 +3,13 @@
 CODEDIR=$(dirname "$0")"/../"
 
 
-#STOCK_FILE_PATH="/home/v2john/Projects/financial-news-dataset/ReutersNews106521/"
-#OUTPUT_FILE_PATH="/home/v2john/reuters_news.txt"
-#
-#/usr/bin/python3 "$CODEDIR"/stock_news_analyzer.py \
-#--stock_news_path "$STOCK_FILE_PATH" \
-#--output_file "$OUTPUT_FILE_PATH" \
-#--news_source reuters
+STOCK_FILE_PATH="/home/v2john/Projects/financial-news-dataset/ReutersNews106521/"
+OUTPUT_FILE_PATH="/home/v2john/reuters_news.txt"
+
+/usr/bin/python3 "$CODEDIR"/stock_news_analyzer.py \
+--stock_news_path "$STOCK_FILE_PATH" \
+--output_file "$OUTPUT_FILE_PATH" \
+--news_source reuters
 
 
 STOCK_FILE_PATH="/home/v2john/Projects/financial-news-dataset/20061020_20131126_bloomberg_news/"
--- utils/file_helper.py
+++ utils/file_helper.py
@@ -20,11 +20,12 @@ def extract_headline_from_file(file_path, news_source):
         with open(file_path) as file:
             all_lines = file.read()
 
+        all_lines = "\n" + all_lines
         all_lines_split = all_lines.split("\n--")
         article_dict = dict()
-        article_dict['headline'] = all_lines_split[0].strip()
-        article_dict['publish_date'] = parse_date(all_lines_split[2].strip(), news_source)
-        article_dict['article_text'] = all_lines[3].strip()
+        article_dict['headline'] = all_lines_split[1].strip()
+        article_dict['publish_date'] = parse_date(all_lines_split[3].strip(), news_source)
+        article_dict['article_text'] = all_lines_split[4].strip()
         article_dict['file_path'] = file_path
 
         if article_dict['headline'] and article_dict['publish_date'] and article_dict['article_text']:
@@ -38,7 +39,7 @@ def parse_date(date_string, news_source):
     if news_source == 'reuters':
         return datetime.strptime(date_string, '%a %b %d, %Y %I:%M%p %Z').timestamp()
     elif news_source == 'bloomberg':
-        datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ").timestamp()
+        return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ").timestamp()
     else:
         log.error("no news source selected " + str(news_source))
         exit(0)
